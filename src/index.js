aa644c7acf9481cd521bfeeb6e8bc322a68f5518
Philip <philipkarpovich@gmail.com>
2017-09-21 16:25:41 +0300

    into array both parameters

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
+  var string = str.split("");
+  var array = [];
 
+  for (var i = 0; i < bracketsConfig.length; i++) {
+    for (var j = 0; j < bracketsConfig[i].length; j++) {
+      array.push(bracketsConfig[i][j])
+    }
+  }
 }
